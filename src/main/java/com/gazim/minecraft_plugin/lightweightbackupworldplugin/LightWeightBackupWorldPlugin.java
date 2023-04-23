267e1e6ed59e7e62e15b4da69df7d012f7ed5d84
Gazim <gazim1304@gmail.com>
2023-04-23 23:01:34 +0500

    save state

---
+++ src/main/java/com/gazim/minecraft_plugin/lightweightbackupworldplugin/LightWeightBackupWorldPlugin.java
@@ -0,0 +1,17 @@
+package com.gazim.minecraft_plugin.lightweightbackupworldplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LightWeightBackupWorldPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
