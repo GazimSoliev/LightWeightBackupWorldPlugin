d9663d2cb466c9d3d8b04ed92c7d8616d1157c7c
Gazim <gazim1304@gmail.com>
2023-04-23 23:12:49 +0500

    save state kts

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+}
+
+group = "com.gazim.minecraft_plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks {
+    processResources {
+        val props = linkedMapOf("version" to version)
+        inputs.properties.putAll(LinkedHashSet())
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
+
+
+kotlin {
+    jvmToolchain(11)
+}
